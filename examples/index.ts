0e2525de485232707ad31103127c2f8e504f940e
gaboesquivel <contact@gaboesquivel.com>
2020-11-20 09:43:52 +0000

    chore: update example to start at latest block height

--- examples/index.ts
+++ examples/index.ts
@@ -1,22 +1,23 @@
 import { EosioShipReaderConfig, ShipBlockResponse } from '../src/types'
 import { ErrorEvent } from 'ws'
 import { createEosioShipReader } from '../src/index'
-// import fetch from 'node-fetch'
+import fetch from 'node-fetch'
 
 const initReader = async () => {
-  // const info = await fetch('http://127.0.0.1:8888/v1/chain/get_info').then((res) => res.json())
-  // console.log(info)
+  const info = await fetch('http://127.0.0.1:8888/v1/chain/get_info').then((res) => res.json())
+  console.log(info)
+
   const eosioShipReaderConfig: EosioShipReaderConfig = {
     ws_url: 'ws://localhost:8080',
     ds_threads: 4,
     ds_experimental: false,
     request: {
-      start_block_num: 152774818,
+      start_block_num: info.head_block_num,
       end_block_num: 0xffffffff,
       max_messages_in_flight: 50,
       have_positions: [],
       irreversible_only: false,
-      fetch_block: true,
+      fetch_block: true,s
       fetch_traces: true,
       fetch_deltas: true,
     },
