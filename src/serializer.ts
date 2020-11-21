29ff835f2f1d6fc46c08480e476e154955a7791a
gaboesquivel <contact@gaboesquivel.com>
2020-11-21 22:14:50 +0000

    wip: cleanup serializer

--- src/serializer.ts
+++ src/serializer.ts
@@ -1,4 +1,3 @@
-// serialization and deserialization helpers
 import { Serialize } from 'eosjs'
 import { TextDecoder, TextEncoder } from 'util'
 import { EosioShipTypes } from './types'
@@ -10,11 +9,3 @@ export const serialize = (type: string, value: Array<string | {}>, types: EosioS
   Serialize.getType(types, type).serialize(buffer, value)
   return buffer.asUint8Array()
 }
-
-// const serializer = new Serialize.SerializerState({ bytesAsUint8Array: true })
-// export function deserialize(type: string, array: Uint8Array, types: Types) {
-//   const buffer = new Serialize.SerialBuffer({ ...encoding, array })
-//   const result = Serialize.getType(types, type).deserialize(buffer, serializer)
-//   if (buffer.readPos !== array.length) throw new Error(`oops: ${type}`)
-//   return result
-// }
