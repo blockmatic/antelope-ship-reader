fc2f28896436f0bf27a12e83ca4bf7921c6b5446
Gabo Esquivel <contact@gaboesquivel.com>
2021-07-21 14:33:35 -0600

    fix: remove console log

--- src/index.ts
+++ src/index.ts
@@ -329,9 +329,6 @@ export const createEosioShipReader = async (config: EosioReaderConfig) => {
         type: 'signed_block_variant',
         data: deserializedShipMessage.block,
       })
-
-      console.log('========== deserializedBlock ==========')
-      console.log(deserializedBlock)
       block.timestamp = deserializedBlock[1].timestamp
       block.producer = deserializedBlock[1].producer
     } else if (state.shipRequest.fetch_block) {
